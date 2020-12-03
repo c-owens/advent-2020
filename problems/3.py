a3676bed402553d99a7a8afff04ef5dbec91f4ce
Cody Owens <owenscody@gmail.com>
2020-12-03 12:01:29 -0600

    Day 3 input and scaffolding

---
+++ problems/3.py
@@ -0,0 +1,24 @@
+
+from .problem import Problem
+
+class Day3(Problem):
+    """
+    Solution for https://adventofcode.com/2020/day/3
+    """
+
+    def __init__(self):
+        Problem.__init__(self)
+
+    def solve_a(self) -> int:
+        pass
+
+    def solve_b(self) -> int:
+        pass
+
+    @property
+    def name(self) -> str:
+        return "Day 3: Toboggan Trajectory"
+
+    @property
+    def number(self) -> int:
+        return 3
